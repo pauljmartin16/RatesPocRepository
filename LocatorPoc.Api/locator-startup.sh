1aae03e41a8d1a4e8a53737e9ad0944c44b5986d
pauljmartin16 <pauljmartin16@gmail.com>
2021-11-16 16:26:46 -0500

    integrating variables into startup

---
+++ LocatorPoc.Api/locator-startup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Surface Vault mount as environment variables
+secretFile="/vault/secrets/secret.env"
+
+if [[ -f "$secretFile" ]]; then
+
+    echo "Detected secret file $secretFile, loading values as environment variables"
+
+    set -o allexport
+    source $secretFile
+    set +o allexport
+fi
+
+# Run the Locator app
+dotnet LocatorPoc.Api.dll
